203012db38c1274270592fc4cb1fc1d9a03863ba
Your Name <aguiarsantosrian@gmail.com>
2026-09-01 10:03:26 -0300

    .

--- videos/celery.py
+++ videos/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meu_projeto.settings")
 
-app = Celery("meu_projeto")
+app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
--- videos/tasks.py
+++ videos/tasks.py
@@ -1,5 +1,4 @@
 import logging
-
 import requests
 from celery import shared_task
 from django.conf import settings
